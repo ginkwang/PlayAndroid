fbe578545fe4716460c927b2e15ffb57e7097fd5
wanggk <wanggk@meixing.com>
2021-01-06 10:06:42 +0800

    完善toolbar

--- app/src/main/java/com/gink/playandroid/ui/main/MainFragment.kt
+++ app/src/main/java/com/gink/playandroid/ui/main/MainFragment.kt
@@ -3,13 +3,13 @@ package com.gink.playandroid.ui.main
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.gink.playandroid.R
 import com.gink.playandroid.databinding.FragmentMainBinding
 import com.gink.playandroid.mvvm.base.BaseFragment
-import com.gink.playandroid.mvvm.ext.initCustomToolbar
+import com.gink.playandroid.mvvm.ext.invisible
+import com.gink.playandroid.mvvm.ext.visible
 import com.gink.playandroid.ui.flow.FlowFragment
 import com.gink.playandroid.ui.me.MeFragment
 import com.gink.playandroid.ui.project.ProjectFragment
@@ -55,17 +55,30 @@ class MainFragment : BaseFragment<MainViewModel, FragmentMainBinding>() {
             registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
-                    Log.i("OnPageChangeCallback", "onPageSelected: " + position)
                     bottom_bar.currentItem = position
+                    if (position < (offscreenPageLimit - 1)) {
+                        showCommonTopbar()
+                    } else {
+                        showMeTopbar()
+                    }
                 }
             })
         }
 
         bottom_bar.setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.menu_flow -> main_view_pager.setCurrentItem(0, false)
-                R.id.menu_project -> main_view_pager.setCurrentItem(1, false)
-                R.id.menu_me -> main_view_pager.setCurrentItem(2, false)
+                R.id.menu_flow -> {
+                    showCommonTopbar()
+                    main_view_pager.setCurrentItem(0, false)
+                }
+                R.id.menu_project -> {
+                    showCommonTopbar()
+                    main_view_pager.setCurrentItem(1, false)
+                }
+                R.id.menu_me -> {
+                    showMeTopbar()
+                    main_view_pager.setCurrentItem(2, false)
+                }
             }
 
             true
@@ -74,5 +87,17 @@ class MainFragment : BaseFragment<MainViewModel, FragmentMainBinding>() {
 
     override fun createObserver() {
 
+    }
+
+    fun showCommonTopbar() {
+        iv_left_icon.visible()
+        ll_search.visible()
+        iv_right_icon.invisible()
+    }
+
+    fun showMeTopbar() {
+        iv_left_icon.invisible()
+        ll_search.invisible()
+        iv_right_icon.visible()
     }
 }
